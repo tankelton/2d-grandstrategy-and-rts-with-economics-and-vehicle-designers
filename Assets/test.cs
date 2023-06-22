2f1035adb97e18b17d31b97ac91419fd7863972f
William Ladds <wjladds@gmail.com>
2023-06-22 15:21:26 +0100

    test

---
+++ Assets/test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    string Test;
+
+    private void Start()
+    {
+        Test = "test";
+    }
+}
